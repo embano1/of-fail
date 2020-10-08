72da2088da01bc57b2d3f67a93e096548f7e34ef
Michael Gasch <mgasch@vmware.com>
2020-10-08 12:31:32 +0200

    Succeed after some failed attempts
    Signed-off-by: Michael Gasch <mgasch@vmware.com>

--- fail/handler.py
+++ fail/handler.py
@@ -1,6 +1,20 @@
+counter = 0
+
 def handle(event, context):
+    global counter
+    counter += 1
+
     print(event.body,flush=True)
+    
+    if counter == 3:
+        # reset counter
+        counter = 0
+        return {
+            "statusCode": 200,
+            "body": f"I finally succeeded after {counter} attempts"
+        }
+        
     return {
-        "statusCode": 400,
-        "body": "I was programmed to fail"
+        "statusCode": 500,
+        "body": f"I was programmed to fail for the {counter} attempt"
     }
